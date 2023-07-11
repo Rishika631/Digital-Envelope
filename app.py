48677e973591dffc465e20b22461222ea82d0ff3
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-12 00:20:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,52 +78,48 @@ def SymmetricDecryption(hashed_msz, encoded_msz, decipher):
     else:
         st.write("--> The hash is different. The data is incorrect")
 
-def Menu():
-    st.write("Please Select the mode of operation:-")
-    st.write("1) Give Input.")
-    st.write("2) Initiate Key Generation Process.")
-    st.write("3) Initiate RSA Encryption For Assymetric Common Key sharing.")
-    st.write("4) Initiate Symmetric Encryption for conversation after key sharing.")
-    st.write("5) Decrypt Asymmetrically shared common key.")
-    st.write("6) Decrypt message sent through Symmetric Conversation.")
-    st.write("7) Exit the program :[.")
-
 def main():
     st.header("RSA Encryption and Decryption")
     die = True
     try:
         while die:
-            Menu()
-            choice = st.number_input("Enter your choice: ", key="menu_choice")
-            if choice == 1:
+            menu_options = ["Select an option", "Give Input", "Initiate Key Generation Process", 
+                            "Initiate RSA Encryption For Assymetric Common Key sharing", 
+                            "Initiate Symmetric Encryption for conversation after key sharing",
+                            "Decrypt Asymmetrically shared common key",
+                            "Decrypt message sent through Symmetric Conversation",
+                            "Exit the program"]
+            choice = st.selectbox("Please Select the mode of operation:", menu_options, key="menu_choice")
+            
+            if choice == "Give Input":
                 st.write("##### DATA INPUT #####")
                 p, q, common_key = Input_data()
                 time.sleep(2)
-            elif choice == 2:
+            elif choice == "Initiate Key Generation Process":
                 st.write("##### KEY GENERATION PROCESS #####")
                 n, e, d = RSAKeyGeneration(p, q)
                 time.sleep(1)
-            elif choice == 3:
+            elif choice == "Initiate RSA Encryption For Assymetric Common Key sharing":
                 st.write("##### RSA ENCRYPTION PROCESS #####")
                 cipher = RSAEncryption(e, n, common_key)
                 time.sleep(1)
-            elif choice == 4:
+            elif choice == "Initiate Symmetric Encryption for conversation after key sharing":
                 st.write("##### SYMMETRIC ENCRYPTION PROCESS #####")
                 hashed_msz, encoded_msz = SymmetricEncryption(common_key)
                 time.sleep(1)
-            elif choice == 5:
+            elif choice == "Decrypt Asymmetrically shared common key":
                 st.write("##### RSA DECRYPTION PROCESS #####")
                 decipher = RSADecryption(n, d, cipher, common_key)
                 time.sleep(1)
-            elif choice == 6:
+            elif choice == "Decrypt message sent through Symmetric Conversation":
                 st.write("##### SYMMETRIC DECRYPTION PROCESS #####")
                 SymmetricDecryption(hashed_msz, encoded_msz, decipher)
                 time.sleep(1)
-            elif choice == 7:
+            elif choice == "Exit the program":
                 die = False
                 st.write("Exiting the program...")
             else:
-                st.write("This choice is not in the menu. Try Again!!!")
+                st.write("Please select a valid option from the menu.")
     except KeyboardInterrupt:
         st.write("\n\nInterrupt received! Exiting cleanly...\n")
 
