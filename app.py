e8b3800d196ebb052f6526718135b373da1ccd2c
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-12 00:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ def CompareHash(hashed_msz, decoded_msz):
     return hashed_msz == decoded_msz
 
 def Input_data():
-    p = st.number_input("Please Enter value of p [Prime number. eg. 7]: ", key="input_p")
-    q = st.number_input("Please Enter value of q [Prime number. eg. 5]: ", key="input_q")
-    common_key = st.number_input("Please enter the common key to be shared: ", key="input_common_key")
+    p = st.number_input("Please Enter value of p [Prime number. eg. 7]: ")
+    q = st.number_input("Please Enter value of q [Prime number. eg. 5]: ")
+    common_key = st.number_input("Please enter the common key to be shared: ")
     st.write("\nThanks!")
     return p, q, common_key
 
@@ -36,7 +36,7 @@ def RSAKeyGeneration(p, q):
     totient_func = (p - 1) * (q - 1)
     flag = 1
     while flag:
-        e = st.number_input("Please select value of e: ", key="input_e")
+        e = st.number_input("Please select value of e: ")
         if gcd(totient_func, e) != 1:
             st.write("\nGCD of totient_func and e should be 1 (Relatively prime).")
             st.write("Please try again!")
@@ -55,7 +55,7 @@ def RSAEncryption(e, n, common_key):
     return cipher
 
 def SymmetricEncryption(common_key):
-    message = st.text_input("Please enter the message to be shared:", key="input_message")
+    message = st.text_input("Please enter the message to be shared:")
     hashed_msz = hashing(message)
     encoded_msz = CaesarCipher(key=common_key).encipher(hashed_msz)
     st.write(f"\nHash for message given is: {hashed_msz}")
@@ -93,7 +93,7 @@ def main():
                     "Decrypt Asymmetrically shared common key",
                     "Decrypt message sent through Symmetric Conversation",
                     "Exit the program",
-                ],
+                ]
             )
 
             if menu_choice == "Give Input":
